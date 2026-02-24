92871fa99ff4e58647f36bf3722a03fddc31ea10
Lect. Çisem YAŞAR <97396158+cyasar34@users.noreply.github.com>
2026-02-24 11:00:01 +0300

    Add files via upload

---
+++ NameForm.jsx
@@ -0,0 +1,9 @@
+function NameForm() {
+  return (
+    <div>
+      <input type="text" placeholder="Adınızı girin" />
+      <button type="button">Gönder</button>
+    </div>
+  );
+}
+export default NameForm;
