2ad58398aba6e94dff1f389e0d803aa5495fdde3
Lect. Çisem YAŞAR <97396158+cyasar34@users.noreply.github.com>
2026-03-03 11:53:29 +0300

    Delete Deneme directory

--- Deneme/NameForm.jsx
+++
@@ -1,9 +0,0 @@
-function NameForm() {
-  return (
-    <div>
-      <input type="text" placeholder="Adınızı girin" />
-      <button type="button">Gönder</button>
-    </div>
-  );
-}
-export default NameForm;
